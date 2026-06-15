ea7f715994c670b2234dccd65f47526cfc935bf3
Saumya Singh <singhsaumyait034@gmail.com>
2026-06-15 07:32:02 +0530

    June Coding Practice Started

---
+++ src/test/java/DataUser.java
@@ -0,0 +1,20 @@
+public class DataUser {
+    private String token;
+    private UserData user;
+
+    public UserData getUser() {
+        return user;
+    }
+
+    public void setUser(UserData user) {
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/test/java/Interview1.java
@@ -0,0 +1,12 @@
+public class Interview1 {
+
+    private DataUser data;
+
+    public DataUser getData() {
+        return data;
+    }
+
+    public void setData(DataUser data) {
+        this.data = data;
+    }
+}
